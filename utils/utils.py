2f020b2f7c71a8a6225c42559b66280dc915d4e0
ecuyle <ecuyle@gmail.com>
2020-11-23 14:34:50 -0800

    Adds core utils for automated thermostat control

---
+++ utils/utils.py
@@ -0,0 +1,9 @@
+import json
+
+def convert_celsius_to_fahrenheit(degrees_in_celsius):
+    return degrees_in_celsius * 9/5.0 + 32
+
+def write_json(data, filename='sensor_data.json'):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4, sort_keys=True, default=str)
+
